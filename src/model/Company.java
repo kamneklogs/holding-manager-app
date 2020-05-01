559adb3b8893f06b08b9f6b5186631f93dda0bbc
paoos9513 <kamneklogs@gmail.com>
2020-04-30 23:06:57 -0500

    Add packages thread and ui

--- src/model/Company.java
+++ src/model/Company.java
@@ -1,6 +1,6 @@
 package model;
 
-import java.util.ArrayList;
+
 
 public class Company {
 
---
+++ src/ui/Main.java
@@ -0,0 +1,12 @@
+package ui;
+
+import model.HoldingMasterClass;
+
+public class Main {
+
+    private HoldingMasterClass theHolding;
+
+    public static void main(String[] args) {
+
+    }
+}
